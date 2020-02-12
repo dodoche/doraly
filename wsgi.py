9b83dcd7ff271e4ec919a3596ea1d2144b7c7301
dodoche <doralyfr@yahoo.fr>
2020-02-12 16:49:20 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,8 +4,6 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-print('OpenShift client version: {}'.format(oc.get_client_version()))
-print('OpenShift server version: {}'.format(oc.get_server_version()))
    return "Jesus lebt!"
 if __name__ == "__main__":
     application.run()
