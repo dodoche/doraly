f1e08e068d6eee95d6321972df9b6503edaa0f09
dodoche <doralyfr@yahoo.fr>
2020-02-12 17:31:55 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 from flask import Flask
 import openshift as oc
-from openshift import openshift_utils
+#from openshift import openshift_utils
 
 print('OpenShift client version: {}'.format(oc.get_client_version()))
 print('OpenShift server version: {}'.format(oc.get_server_version()))
