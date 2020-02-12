68a0fc51724d1beba0bf9bfbdb0d1efaddf18f00
dodoche <doralyfr@yahoo.fr>
2020-02-12 20:42:33 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,33 @@
-#!/usr/bin/python
-from flask import Flask
-import openshift as oc
-#from openshift import openshift_utils
+from kubernetes import client, config, watch
 
-print(oc get pods)
-#print('OpenShift server version: {}'.format(oc.get_server_version()))
 
-application = Flask(__name__)
+def main():
+    # Configs can be set in Configuration class directly or using helper
+    # utility. If no argument provided, the config will be loaded from
+    # default location.
+    config.load_kube_config()
 
-@application.route("/")
-def hello():
-   return "Jesus lebt!"
-if __name__ == "__main__":
-    application.run()
+    v1 = client.CoreV1Api()
+    count = 10
+    w = watch.Watch()
+    for event in w.stream(v1.list_namespace, timeout_seconds=10):
+        print("Event: %s %s" % (event['type'], event['object'].metadata.name))
+        count -= 1
+        if not count:
+            w.stop()
+    print("Finished namespace stream.")
+
+    for event in w.stream(v1.list_pod_for_all_namespaces, timeout_seconds=10):
+        print("Event: %s %s %s" % (
+            event['type'],
+            event['object'].kind,
+            event['object'].metadata.name)
+        )
+        count -= 1
+        if not count:
+            w.stop()
+    print("Finished pod stream.")
+
+
+if __name__ == '__main__':
+    main()
