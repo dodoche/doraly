bcb7e323c3a8c17a9a3370adabbc1ddd707a3847
dodoche <doralyfr@yahoo.fr>
2020-02-24 22:32:52 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,17 @@
 from flask import Flask
+import yaml
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
+    
+    with open(r'docker-compose.yml') as file:
+    # The FullLoader parameter handles the conversion from YAML
+    # scalar values to Python the dictionary format
+    documents = yaml.load(file)
+    for item, doc in documents.items():
+        print(item, ":", doc)
+
     return "Hello World!"
 
 if __name__ == "__main__":
